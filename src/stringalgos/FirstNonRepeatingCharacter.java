9b503dd3c4321934ed5304c08df620b9effb97eb
sushanthbangera <30630541+sushanthbangera@users.noreply.github.com>
2017-08-22 23:55:08 +0530

    Optimized Non repeating character method
    Optimized by reducing the iteration.

--- src/stringalgos/FirstNonRepeatingCharacter.java
+++ src/stringalgos/FirstNonRepeatingCharacter.java
@@ -10,9 +10,8 @@ import utils.CountIndex;
  */
 public class FirstNonRepeatingCharacter {
 
-    static Map<Character, CountIndex> charCountMap = new HashMap<>();
-
-    public static void storeCharacterCount(String str) {
+    // To store the count of each character in a String
+    private static void storeCharacterCount(String str, Map<Character, CountIndex> charCountMap) {
         char[] charArray = str.toCharArray();
         int len = charArray.length;
         for (int i = 0; i < len; i++) {
@@ -25,26 +24,27 @@ public class FirstNonRepeatingCharacter {
         }
     }
 
+    // Returns the index of the first non repeating character else return Integer max value
     public static int getFirstNonRepeatingCharacterIndex(String str) {
-        if(str == null) {
+        if (str == null) {
             return Integer.MAX_VALUE;
         }
-        storeCharacterCount(str);
-        int len = str.length(), result = Integer.MAX_VALUE, i;
+        Map<Character, CountIndex> charCountMap = new HashMap<>();
+        storeCharacterCount(str, charCountMap);
+        int result = Integer.MAX_VALUE;
 
-        for (i = 0; i < len; i++) {
-            char c = str.charAt(i);
-            if (charCountMap.get(c).count == 1 && result > charCountMap.get(c).index) {
-                result = charCountMap.get(c).index;
+        for (Map.Entry<Character, CountIndex> cmap : charCountMap.entrySet()) {
+            if (cmap.getValue().count == 1 && result > cmap.getValue().index) {
+                result = cmap.getValue().index;
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        String str = "sushanth";
+        String str = "ssuuh";
         int index = getFirstNonRepeatingCharacterIndex(str);
-        System.out.println("index == "+index);
+        System.out.println("index == " + index);
         System.out.println(index == Integer.MAX_VALUE ? "All are repeating/Empty String" : "First Non Repeating Character is " + str.charAt(index));
     }
 
